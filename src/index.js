2c039da28ef5c342e85ac1c9d343e2cd46963277
baldx <mr.ahmad.m2006@gmail.com>
2023-08-22 19:18:01 +0200

    change name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const input = document.querySelector('#search');
 const submit = document.querySelector('#submit');
-const p = document.querySelector('.weather');
+const degrees = document.querySelector('.degrees');
 let api;
 
 submit.addEventListener('click', (e) => {
@@ -17,9 +17,9 @@ async function showData () {
         const convert = await response.json()
         console.log(convert);
 
-        p.innerHTML = convert.current.temp_c + 'C'
+        degrees.innerHTML = convert.current.temp_c + 'C'
     } catch(err) {
-        p.innerHTML = 'No matching location found'
+        degrees.innerHTML = 'No matching location found'
     }
 }
 
